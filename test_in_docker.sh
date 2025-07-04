a3a32e650651a3172b6465397db197f3e02be68b
Timur Osmanov <timurosm@yandex.ru>
2025-07-04 12:07:32 +0300

    update: script for run tests

--- test_in_docker.sh
+++ test_in_docker.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# # Write Dockerfile
-echo "FROM python:3.7" > Dockerfile
-echo "RUN pip install foliantcontrib.utils" >> Dockerfile
-echo "RUN pip install requests" >> Dockerfile
+PYTHON_VERSIONS=("3.8" "3.9")
 
-# Run tests in docker
-docker build . -t test-foliant:latest
+for version in "${PYTHON_VERSIONS[@]}"; do
+    # Write Dockerfile
+    echo "FROM python:${version}" > Dockerfile
+    echo "RUN pip install foliantcontrib.utils" >> Dockerfile
+    echo "RUN pip install requests" >> Dockerfile
 
-docker run --rm -it -v "./:/app/" -w /app/ --entrypoint /bin/bash test-foliant:latest -l -c "./test.sh"
+    # Run tests in docker
+    docker build . -t test-foliant:latest
 
-# Remove Dockerfile
-rm Dockerfile
+    docker run --rm -it -v "./:/app/" -w /app/ --entrypoint /bin/bash test-foliant:latest -l -c "./test.sh"
+
+    # Remove Dockerfile
+    rm Dockerfile
+    docker rmi test-foliant:latest
+done
